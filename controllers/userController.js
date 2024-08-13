112819b4041cd38e4f21e92c50817b435dd51d58
galtiparmak <115663609+galtiparmak@users.noreply.github.com>
2024-08-13 20:57:30 +0300

    Update UserContoller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,7 @@
 const { User, Book, UserBookHistory } = require('../models');
 const userSchema = require('../schemas/userSchema');
 const scoreSchema = require('../schemas/scoreSchema');
+const sequelize = require('../config/database');
 
 // ... rest of the controller code
 
@@ -57,82 +58,104 @@ const createUser = async (req, res, next) => {
 };
 
 const borrowBook = async (req, res, next) => {
+    const transaction = await sequelize.transaction();
+
     try {
-        const user = await User.findByPk(req.params.userId);
-        const book = await Book.findByPk(req.params.bookId);
+        const user = await User.findByPk(req.params.userId, { transaction });
+        const book = await Book.findByPk(req.params.bookId, { transaction });
 
         if (!user) {
+            await transaction.rollback();
             return res.status(404).json({ error: 'User not found' });
         }
-
+    
         if (!book) {
+            await transaction.rollback();
             return res.status(404).json({ error: 'Book not found' });
         }
 
         const isAlreadyBorrowed = await user.sequelize.models.UserCurrentBooks.findOne({
-            where: { BookId: book.id }
+            where: { BookId: book.id },
+            transaction
         });
 
         if (isAlreadyBorrowed) {
+            await transaction.rollback();
             return res.status(400).json({ error: 'This book is currently borrowed by another user' });
         }
 
-        await user.addCurrentlyBorrowedBooks(book);
+        await user.addCurrentlyBorrowedBooks(book, { transaction });
 
-        res.status(204).send();  // No Content
+        await transaction.commit();
+        res.status(204).send();
     } catch (err) {
+        await transaction.rollback();
         next(err);
     }
 };
 
 const returnBook = async (req, res, next) => {
-    try {
-        const { error } = scoreSchema.validate(req.body);
-        if (error) {
-            return res.status(400).json({ error: error.details[0].message });
-        }
+    const transaction = await sequelize.transaction();
 
-        const user = await User.findByPk(req.params.userId);
-        const book = await Book.findByPk(req.params.bookId);
-
-        if (!user || !book) {
-            return res.status(404).json({ error: 'User or Book not found' });
-        }
-
-        const isBorrowed = await user.hasCurrentlyBorrowedBooks(book);
-        if (!isBorrowed) {
-            return res.status(400).json({ error: 'This book has not been borrowed by the user or has already been returned' });
-        }
-
-        await user.removeCurrentlyBorrowedBooks(book);
-
-        const latestBorrowing = await UserBookHistory.findOne({
-            where: {
-                UserId: user.id,
-                BookId: book.id,
-                returnedAt: null
-            },
-            order: [['borrowedAt', 'DESC']]
-        });
-
-        if (latestBorrowing) {
-            latestBorrowing.returnedAt = new Date();
-            latestBorrowing.score = req.body.score;
-            await latestBorrowing.save();
-        } else {
-            await UserBookHistory.create({
-                UserId: user.id,
-                BookId: book.id,
-                borrowedAt: new Date(),
-                returnedAt: new Date(),
-                score: req.body.score,
-            });
-        }
-
-        res.status(204).send();  // No Content
-    } catch (err) {
-        next(err);
+try {
+    const { error } = scoreSchema.validate(req.body);
+    if (error) {
+        await transaction.rollback();
+        return res.status(400).json({ error: error.details[0].message });
     }
+
+    const user = await User.findByPk(req.params.userId, { transaction });
+    const book = await Book.findByPk(req.params.bookId, { transaction });
+
+    if (!user) {
+        await transaction.rollback();
+        return res.status(404).json({ error: 'User not found' });
+    }
+
+    if (!book) {
+        await transaction.rollback();
+        return res.status(404).json({ error: 'Book not found' });
+    }
+
+    const isBorrowed = await user.hasCurrentlyBorrowedBooks(book, { transaction });
+    if (!isBorrowed) {
+        await transaction.rollback();
+        return res.status(400).json({ error: 'This book has not been borrowed by the user or has already been returned' });
+    }
+
+    await user.removeCurrentlyBorrowedBooks(book, { transaction });
+
+    const latestBorrowing = await UserBookHistory.findOne({
+        where: {
+            UserId: user.id,
+            BookId: book.id,
+            returnedAt: null
+        },
+        order: [['borrowedAt', 'DESC']],
+        transaction
+    });
+
+    if (latestBorrowing) {
+        latestBorrowing.returnedAt = new Date();
+        latestBorrowing.score = req.body.score;
+        await latestBorrowing.save({ transaction });
+    } else {
+        await UserBookHistory.create({
+            UserId: user.id,
+            BookId: book.id,
+            borrowedAt: new Date(),
+            returnedAt: new Date(),
+            score: req.body.score,
+        }, { transaction });
+    }
+
+    await transaction.commit();
+    res.status(204).send();
+} catch (err) {
+    await transaction.rollback();
+    next(err);
+}
+
 };
 
 module.exports = {
